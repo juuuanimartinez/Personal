2faeef37e7b84ced2184c006c19abd6f901dbef6
Juuanimartinez <juuuanimartinez@gmail.com>
2022-04-27 10:47:38 -0300

    ok

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,8 +3,8 @@ const moment = require("moment");
 const User = require("../models/user");
 
 function willExpiredToken(token)
-{
-    const {exp}=jwt.decodedToken(token)
+{ const poncho="";
+        const {exp}=jwt.decodedToken(token)
     const currentDate = moment().unix();
 
     if( currentDate>exp)
@@ -21,7 +21,21 @@ function refreshAccessToken(req, res)
  {
      const {refreshToken}=req.body;
      const isTokenExpired=willExpiredToken(refreshToken);
-     console.log(isTokenExpired);
+     if(isTokenExpired)
+     {
+         res.statur(404).send({message:"El refresh token esta expirado"});
+
+     }
+     else{
+         const{id}=jwt.decodedToken(refreshToken);
+         User.findOne({_id:id},(err,userStorage)=>
+         {
+             if(err)
+             {
+                 res.status(500).send({message:"Error en el servidor"});
+             }
+         })
+     }
  }
  module.exports = {
     refreshAccessToken
